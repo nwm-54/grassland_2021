68f59da8e3e50d9810fbb236e650afb902af6184
nwm-54 <trannguyenthanhvi98@gmail.com>
2021-02-03 02:59:43 -0800

    test ingramcreek

--- weekly_report/report_debug_feb2.py
+++ weekly_report/report_debug_feb2.py
@@ -11,24 +11,31 @@ def main():
 
 
 	for station_info in [ingramcreek_info]:
-		dirname_input = '/home/nwtquinn/public_ftp/incoming/sjvda-realtime.org/SJVDA/'+station_info['name']+'/'
+		dirname_input = '/home/vitran/ingram/'#'/home/nwtquinn/public_ftp/incoming/sjvda-realtime.org/SJVDA/'+station_info['name']+'/'
 		dirname_stats_output = None #'/home/nwtquinn/public_ftp/daily_report/SJVDA_reports/'+station_info['name']+'/'+station_info['name']+'_stats.csv' 
     
-		today = date.today()
+		# today = date.today()
 # 		start_date = Date(8,14,2020)
 # 		end_date = Date(1,31,2021)
 
- 		cur_date = Date(1,31,2021)
-#		cur_date = Date(today.day, today.month, today.year)-1
+		cur_date = Date(1,31,2021)
+		tmr = cur_date.next_date()
+		next_tmr = tmr.next_date()
+		print(next_tmr+1)
+		# cur_date = Date(today.day, today.month, today.year)-1
 		cur_date_output_file_name = file_name_from_date(cur_date)
-		dirname_raw_output =  '/home/nwtquinn/public_ftp/daily_report/code/weekly_report/output/'+station_info['name']+'/daily_raw_data/'+station_info['name']+cur_date_output_file_name+'.csv'
+		dirname_raw_output =  '/home/vitran/server_code/weekly_report/output/ingramcreek_debug_feb2/'+station_info['name']+cur_date_output_file_name+'.csv' #'/home/nwtquinn/public_ftp/daily_report/code/weekly_report/output/'+station_info['name']+'/daily_raw_data/'+station_info['name']+cur_date_output_file_name+'.csv'
 		dirname_in_out = {'input': dirname_input, 
     					'raw_output':dirname_raw_output,
     					'stats_output': dirname_stats_output}
     					
 		my_station = Station(cur_date, station_info, dirname_in_out)
-		my_station.collect_raw_data()
-#		my_station.calc_daily_stats()
+		raw_data = my_station.collect_raw_data()
+		write_to_csv(raw_data,output=dirname_raw_output, mode='s')
+
+		# daily_stats = my_station.calc_daily_stats()
+		# if date_not_inserted(daily_stats,dirname_stats_output):
+			# write_to_csv(daily_stats,output=dirname_stats_output, mode='a')
 # 			cur_date +=1
 	
 	
